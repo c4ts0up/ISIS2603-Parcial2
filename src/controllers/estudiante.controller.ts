6c44e35b32b80f16e4d7d91d7d5fdd2e3106ac8b
alvaro <a.baccap@uniandes.edu.co>
2025-05-17 11:23:34 -0500

    Controlador de Estudiante

---
+++ src/controllers/estudiante.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { EstudianteService } from '../services/estudiante.service';
+import { EstudianteDto } from '../dtos/estudiante.dto';
+
+@Controller('estudiante')
+export class EstudianteController {
+  constructor(private readonly estudianteService: EstudianteService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  crearEstudiante(@Body() estudianteDto: EstudianteDto) {
+    return this.estudianteService.crearEstudiante(estudianteDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post(':/estudianteId/actividades/:actividadId')
+  inscribirseActividad(@Param('estudianteId') estudianteId: number, @Param('actividadId') actividadId: number) {
+    return this.estudianteService.inscribirseActividad(
+      estudianteId,
+      actividadId,
+    );
+  }
+}
