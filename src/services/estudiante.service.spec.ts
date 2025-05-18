cf37aa9cb1e983d532e747950cae24e25241c7a7
alvaro <a.baccap@uniandes.edu.co>
2025-05-18 13:49:48 -0500

    Tests de EstudianteService completos

--- src/services/estudiante.service.spec.ts
+++ src/services/estudiante.service.spec.ts
@@ -6,6 +6,8 @@ import { EstudianteDto } from '../dtos/estudiante.dto';
 import { EstudianteService } from './estudiante.service';
 import { ActividadEntity } from '../entities/actividad.entity';
 import { ActividadService } from './actividad.service';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { ActividadDto } from '../dtos/actividad.dto';
 
 describe('EstudianteService', () => {
   let service: EstudianteService;
@@ -13,12 +15,15 @@ describe('EstudianteService', () => {
   let actividadRepository: Repository<ActividadEntity>;
   let actividadService: ActividadService;
 
+  let mockEstudianteDto: EstudianteDto;
+  let mockEstudianteEntity: EstudianteEntity;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ActividadService,
         EstudianteService,
-        { provide: getRepositoryToken(ActividadEntity), useValue: Repository },
+        { provide: getRepositoryToken(ActividadEntity), useClass: Repository },
         { provide: getRepositoryToken(EstudianteEntity), useClass: Repository },
       ],
     }).compile();
@@ -31,39 +36,163 @@ describe('EstudianteService', () => {
     estudianteRepository = module.get<Repository<EstudianteEntity>>(
       getRepositoryToken(EstudianteEntity),
     );
+
+    jest.clearAllMocks();
+
+    mockEstudianteDto = {
+      numeroCedula: 1000222333,
+      nombre: 'Hari Seldon',
+      correo: 'hari.seldon@mail.com',
+      programa: 'Psicohistoria',
+      semestre: 6,
+    };
+
+    mockEstudianteEntity = {
+      id: 1,
+      resenas: [],
+      actividades: [],
+      ...mockEstudianteDto,
+    };
+
+    jest
+      .spyOn(estudianteRepository, 'create')
+      .mockReturnValue(mockEstudianteDto as EstudianteEntity);
+    jest
+      .spyOn(estudianteRepository, 'save')
+      .mockResolvedValue(mockEstudianteEntity);
+    jest
+      .spyOn(estudianteRepository, 'findOne')
+      .mockResolvedValue(mockEstudianteEntity);
   });
 
   describe('crearEstudiante', () => {
-    let mockEstudianteDto: EstudianteDto;
-    let mockEstudianteEntity: EstudianteEntity;
-
-    beforeEach(() => {
-      mockEstudianteDto = {
-        numeroCedula: 1000222333,
-        nombre: 'Hari Seldon',
-        correo: 'hari.seldon@mail.com',
-        programa: 'Psicohistoria',
-        semestre: 6,
-      };
-
-      mockEstudianteEntity = {
-        id: 1,
-        resenas: [],
-        actividades: [],
-        ...mockEstudianteDto,
-      };
-
-      jest
-        .spyOn(estudianteRepository, 'save')
-        .mockResolvedValue(mockEstudianteEntity);
-    });
-
     it('estudiante creado exitosamente', async () => {
       const result = await service.crearEstudiante(mockEstudianteDto);
 
       // eslint-disable-next-line @typescript-eslint/unbound-method
       expect(estudianteRepository.save).toHaveBeenCalledWith(mockEstudianteDto);
       expect(result).toBe(mockEstudianteEntity);
+    });
+  });
+
+  describe('findEstudianteById', () => {
+    it('existe', async () => {
+      const result = await service.findEstudianteById(mockEstudianteEntity.id);
+
+      expect(result).toBe(mockEstudianteEntity);
+    });
+
+    it('no existe', async () => {
+      jest.spyOn(estudianteRepository, 'findOne').mockResolvedValue(null);
+
+      await expect(
+        service.findEstudianteById(mockEstudianteEntity.id),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('inscribirseActividad', () => {
+    let mockActividad: ActividadDto;
+    let mockActividadEntity: ActividadEntity;
+
+    beforeEach(() => {
+      mockActividad = {
+        titulo: 'Plastilina Galáctica',
+        fecha: '2100-11-30',
+        cupoMaximo: 5,
+        estado: 0,
+      };
+
+      mockActividadEntity = {
+        ...mockActividad,
+        id: 1,
+        estudiantes: [],
+        resenas: [],
+      };
+
+      jest
+        .spyOn(actividadRepository, 'findOne')
+        .mockResolvedValue(mockActividadEntity);
+      jest
+        .spyOn(actividadRepository, 'save')
+        .mockResolvedValue(mockActividadEntity);
+    });
+
+    it('inscripción exitosa', async () => {
+      await service.inscribirseActividad(
+        mockEstudianteEntity.id,
+        mockActividadEntity.id,
+      );
+
+      expect(mockEstudianteEntity.actividades).toContain(mockActividadEntity);
+      expect(mockActividadEntity.estudiantes).toContain(mockEstudianteEntity);
+    });
+
+    it('no hay cupos en la actividad', async () => {
+      mockActividadEntity.cupoMaximo = 0;
+
+      await expect(
+        service.inscribirseActividad(
+          mockEstudianteEntity.id,
+          mockActividadEntity.id,
+        ),
+      ).rejects.toThrow(ConflictException);
+
+      expect(mockEstudianteEntity.actividades).not.toContain(
+        mockActividadEntity,
+      );
+      expect(mockActividadEntity.estudiantes).not.toContain(
+        mockEstudianteEntity,
+      );
+    });
+
+    it('la actividad no está en estado 0 (abierta)', async () => {
+      mockActividadEntity.estado = 1;
+
+      await expect(
+        service.inscribirseActividad(
+          mockEstudianteEntity.id,
+          mockActividadEntity.id,
+        ),
+      ).rejects.toThrow(ConflictException);
+
+      expect(mockEstudianteEntity.actividades).not.toContain(
+        mockActividadEntity,
+      );
+      expect(mockActividadEntity.estudiantes).not.toContain(
+        mockEstudianteEntity,
+      );
+    });
+
+    it('el estudiante estaba inscrito previamente', async () => {
+      mockActividadEntity.estudiantes.push(mockEstudianteEntity);
+      mockEstudianteEntity.actividades.push(mockActividadEntity);
+
+      await expect(
+        service.inscribirseActividad(
+          mockEstudianteEntity.id,
+          mockActividadEntity.id,
+        ),
+      ).rejects.toThrow(ConflictException);
+
+      expect(
+        mockEstudianteEntity.actividades.filter((actividad) =>
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          expect
+            .objectContaining({ id: mockActividadEntity.id })
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            .asymmetricMatch(actividad),
+        ).length,
+      ).toBe(1);
+      expect(
+        mockActividadEntity.estudiantes.filter((estudiante) =>
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          expect
+            .objectContaining({ id: mockEstudianteEntity.id })
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            .asymmetricMatch(estudiante),
+        ).length,
+      ).toBe(1);
     });
   });
 });
--- src/services/estudiante.service.ts
+++ src/services/estudiante.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { EstudianteEntity } from '../entities/estudiante.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -19,8 +23,9 @@ export class EstudianteService {
   async crearEstudiante(
     estudianteDto: EstudianteDto,
   ): Promise<EstudianteEntity> {
+    const estudiante = this.estudianteRepository.create(estudianteDto);
     // reglas de validación en el DTO
-    return await this.estudianteRepository.save(estudianteDto);
+    return await this.estudianteRepository.save(estudiante);
   }
 
   async findEstudianteById(idEstudiante: number): Promise<EstudianteEntity> {
@@ -42,6 +47,18 @@ export class EstudianteService {
     const estudiante = await this.findEstudianteById(estudianteId);
     const actividad =
       await this.actividadService.findActividadById(actividadId);
+
+    if (actividad.estado !== 0) {
+      throw new ConflictException(
+        'La actividad no está abierta para inscripciones.',
+      );
+    } else if (actividad.cupoMaximo <= actividad.estudiantes.length) {
+      throw new ConflictException('La actividad no tiene cupos disponibles.');
+    } else if (actividad.estudiantes.includes(estudiante)) {
+      throw new ConflictException(
+        'El estudiante ya está inscrito en la actividad.',
+      );
+    }
 
     estudiante.actividades.push(actividad);
     actividad.estudiantes.push(estudiante);
